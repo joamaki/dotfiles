d88e85236c087de913062630cabaaaa17e9a01d9
Jussi Maki <jussi@isovalent.com>
2021-04-01 10:29:49 +0200

    Drop kakoune, zsh options
    Dropped kakoune as it makes more sense to build it from source.
    Will need to automate that still.

--- run_once_install_golang_tools.sh
+++ run_once_install_golang_tools.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+export PATH=$PATH:$HOME/go/bin
 
 # golangci-lint
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | \
@@ -10,4 +11,7 @@ go get -u github.com/onsi/ginkgo/ginkgo
 
 # gomega
 go get -u github.com/onsi/gomega
+
+# gopls, the go language server.
+GO111MODULE=on go get golang.org/x/tools/gopls@latest
 
